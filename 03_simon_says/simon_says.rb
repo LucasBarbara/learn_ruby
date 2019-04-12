d0334c933a0b0467dce06de95bb4c37c40f1cc89
Barbara <barbara.f.a.lucas@gmail.com>
2019-04-12 22:20:56 +0100

    Add solution for simon_says exercise

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,45 @@
 #write your code here
+
+def echo(word)
+  word
+end
+ 
+def shout(word)
+  word.upcase
+end
+
+def repeat(word, x = 2)
+    word = (word + ' ') * x
+    word = word.chomp(' ')
+end
+
+def start_of_word(string, x = 1)
+    char = string[0, x]
+end
+
+def first_word(string)
+  word = string.split(' ').first
+end
+
+def titleize(string)
+
+  if !string.include? ' '
+    string.capitalize
+  else
+    words = string.split(' ')
+    i = 0
+
+    while i < words.length do
+      if (words[i].length.to_i > 3 && words[i] != 'over' || i == 0)
+        words[i] = words[i].capitalize 
+      end
+      i += 1
+    end
+
+    words.join(' ')
+  end
+
+end
+
+puts titleize('helld the world')
+
