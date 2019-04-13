3f20677ab2c84e1c0581cc9956a6cbc06a325396
Barbara <barbara.f.a.lucas@gmail.com>
2019-04-13 01:03:24 +0100

    Solve pig_latin exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,21 @@
 #write your code here
+ def translate(string)
+
+  words = string.split(' ')
+
+  words = words.map do |word|
+    if word.include? 'qu'
+      x = word.index('qu') + 2
+      array = [word.slice(0, x), word.slice(x, word.length - 2)] 
+    else
+      array = word.split(/([aeiou].*)/)
+    end
+    array[1] + array[0] + 'ay'
+  end
+  
+  words.join (' ')
+ end
+
+
+puts translate('the quake')
+
