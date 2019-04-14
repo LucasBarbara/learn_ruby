e83b5314841badafde0d8874126218c7c7407406
Barbara <barbara.f.a.lucas@gmail.com>
2019-04-14 19:42:30 +0100

    Add solution for timer exercise

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -8,13 +8,35 @@ class Timer
   end
 
   def time_string
-    hours = @seconds/3600
-    remainder = @seconds%3600
-    sprintf("%02d:%02d:%02d", hours, remainder/60, remainder%60)
+
+    hours = @seconds / 3600
+    if hours < 10
+      hours = '0' + hours.to_s
+      puts hours
+    else
+      hours = hours.to_s
+    end
+  
+    minutes = @seconds % 3600 / 60
+    if minutes < 10
+      minutes = '0' + minutes.to_s
+      puts minutes
+    else
+      minutes = minutes.to_s
+    end
+
+    secs = @seconds % 3600 % 60
+    if secs < 10
+      secs = '0' + secs.to_s
+      puts secs
+    else
+      secs = secs.to_s
+    end
+      hours + ':' + minutes + ':' + secs
   end
 
 end
 
 @timer = Timer.new
-@timer.seconds = 2
+@timer.seconds = 4000
 puts @timer.time_string
